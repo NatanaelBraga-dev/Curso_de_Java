adb831ef221f430df0a3012a0dcb2fe45d256756
NatanaelBraga-dev <142696538+NatanaelBraga-dev@users.noreply.github.com>
2026-01-08 22:22:55 -0300

    exercicio envolvendo operadores condicionais

---
+++ src/academy/devdojo/maratonajava/introducao/exercicioEstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class exercicioEstruturasCondicionais {
+    public static void main(String[] args) {
+        //exercicio baseado na taxa salarial holandesa
+        //será feito um cálculo de acordo com o salário
+        double salario = 10000.00;
+        double porcentagemDeImposto;
+
+        if (salario > 0 && salario <= 34712) {
+            porcentagemDeImposto = 9.70 / 100;
+        } else if (salario <= 68507) {
+            porcentagemDeImposto = 37.35 / 100;
+        } else {
+            porcentagemDeImposto = 49.00 / 100;
+        }
+
+        double valorAPagarDeImposto = salario * porcentagemDeImposto;
+
+        System.out.printf("o valor a pagar de imposto é: %.2f%n ", valorAPagarDeImposto );
+        System.out.printf("salário liquido: %.2f ", salario - valorAPagarDeImposto);
+    }
+}
