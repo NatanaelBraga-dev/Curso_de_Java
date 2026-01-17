cf933bde758369018a93026c7026ec812de5a924
NatanaelBraga-dev <natanaelcordeirobraga@gmail.com>
2026-01-17 19:04:01 -0300

    estruturas de repeticao while - do while e for

---
+++ src/academy/devdojo/maratonajava/introducao/aula06EstruturaDeRepeticao02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula06EstruturaDeRepeticao02 {
+    public static void main(String[] args) {
+        //imprima todos os numeros pares do 0 até 100
+        for(int i = 0; i <=100; i+=1){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
