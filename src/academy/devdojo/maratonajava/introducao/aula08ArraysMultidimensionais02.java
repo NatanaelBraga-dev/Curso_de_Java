5e639ec1de65a5e6e2d46011e476b1330097276a
NatanaelBraga-dev <natanaelcordeirobraga@gmail.com>
2026-01-19 21:28:56 -0300

    aprendendo sobre arrays multidimencionais e como interagir com eles

---
+++ src/academy/devdojo/maratonajava/introducao/aula08ArraysMultidimensionais02.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula08ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int [][] arrayInt = new int [3][];
+        int [] array = {1,2,3};
+
+        arrayInt[0] = new int [2];
+        arrayInt[1] = array ;
+        arrayInt[2] = new int [4];
+
+        for(int [] arrBase : arrayInt){
+            for (int num : arrBase){
+                System.out.println(num);
+            }
+        }
+
+        int [][] arrayInt2 = {{1,2,3}, {1,2,3,4}, {1,2,3,4,5,6}};
+        for(int [] arrayBase: arrayInt2){
+            System.out.println("\n----------");
+            for(int num : arrayBase){
+                System.out.println(num);
+            }
+        }
+    }
+}
