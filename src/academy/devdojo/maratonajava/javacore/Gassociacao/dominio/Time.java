9cb24bc4e7fdb5f36a55504d064e8a19a13734b9
NatanaelBraga-dev <natanaelcordeirobraga@gmail.com>
2026-02-28 22:04:05 -0300

    associacao bidirecional

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
@@ -2,9 +2,36 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Time {
     private String nome;
+    private Jogador [] jogadores;
+
 
     public Time (String nome){
         this.nome = nome;
+    }
+
+    public Time(Jogador[] jogadores, String nome) {
+        this.jogadores = jogadores;
+        this.nome = nome;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if(jogadores == null){
+            return;
+        }
+        else{
+            for (Jogador jogador: jogadores){
+                System.out.println(jogador.getNome());
+            }
+        }
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
     }
 
     public String getNome() {
