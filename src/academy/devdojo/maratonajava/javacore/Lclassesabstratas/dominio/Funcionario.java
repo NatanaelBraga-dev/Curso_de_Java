19612f78b31079d86d5ddb61b2661ecbc083e79c
NatanaelBraga-dev <natanaelcordeirobraga@gmail.com>
2026-03-16 18:56:04 -0300

    entendendo classes abstratas

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
+
+public abstract class Funcionario extends Pessoa {
+    protected String nome;
+    protected double salario;
+
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    @Override
+    public void imprime() {
+        System.out.println("imprimindo");
+    }
+
+    public abstract void calculaBonus();
+
+}
